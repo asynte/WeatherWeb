8143a88795382c5b6f2a318f28bb5cdca2e0adbe
Leeleo3x <leeleo3x@gmail.com>
2015-04-27 14:05:26 +0800

    增加了数据下载的接口

--- js/main.js
+++ js/main.js
@@ -8,11 +8,30 @@ function datetimePickerClicked() {
 }
 
 function submitClicked() {
+    $("#fieldError").hide();
+
     date = $("#datetimepicker1").datepicker('getFormattedDate').split("/");
     day = date[0];
-    month = date[0];
-    longitude = $("#longitude").val();
-    latitude = $("#latitude").val();
+    month = date[1];
+    longitude = Number($("#longitude").val());
+    latitude = Number($("#latitude").val());
     console.log(longitude);
+    console.log(typeof longitude);
     console.log(latitude);
+    console.log(typeof latitude);
+
+    if (isNaN(day) || isNaN(month) || isNaN(longitude) || isNaN(latitude)) {
+        $("#fieldError").show();
+    }
+    else {
+        request.getWeatherData(latitude, longitude, month, day, function (err, data) {
+            if (err || data[code] !== 0) {
+                $("#fieldError").val("数据加载错误");
+                $("#fieldError").show();
+            }
+            else {
+
+            }
+        });
+    }
 }
